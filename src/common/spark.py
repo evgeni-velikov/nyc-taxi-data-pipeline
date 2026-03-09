00a39c3f1102054f99142830cd20b8d0808a0f61
evgeni-velikov <evgeni.ivanov.velikov@gmail.com>
2026-03-09 09:03:12 +0200

    upgrade spark and env.example and docker-compose for aws ec2

--- src/common/spark.py
+++ src/common/spark.py
@@ -2,13 +2,8 @@ import os
 from pyspark.sql import SparkSession
 from delta import configure_spark_with_delta_pip
 
-from src.common.environment import require_env
-
 
 def get_spark_session(app_name: str = "nyc-taxi") -> SparkSession:
-    access_key = require_env("AWS_ACCESS_KEY_ID")
-    secret_key = require_env("AWS_SECRET_ACCESS_KEY")
-
     master = os.environ.get("SPARK_MASTER", "local[*]")
     s3_endpoint = os.environ.get("S3_ENDPOINT", "s3.amazonaws.com")
 
@@ -32,14 +27,11 @@ def get_spark_session(app_name: str = "nyc-taxi") -> SparkSession:
         # S3
         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
         .config("spark.hadoop.fs.s3a.endpoint", s3_endpoint)
-        .config("spark.hadoop.fs.s3a.access.key", access_key)
-        .config("spark.hadoop.fs.s3a.secret.key", secret_key)
         .config(
             "spark.hadoop.fs.s3a.aws.credentials.provider",
-            "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
+            "com.amazonaws.auth.InstanceProfileCredentialsProvider",
         )
     )
 
     spark = configure_spark_with_delta_pip(builder).getOrCreate()
-
     return spark
